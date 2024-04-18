fa72b7ae90f020031244bdae929aa74af379f0b8
PabloKoerich <158224802+PabloKoerich@users.noreply.github.com>
2024-04-18 14:25:38 -0300

    Add files via upload

---
+++ ProgramEX01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Solução_01_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Volume | Academia de Programação 2024!\n");
+
+            Console.WriteLine("Para calcular o volume, precisamos de algumas informações. " +
+                "Por favor, insira as dimensões do objeto (comprimento x largura x altura).");
+
+            double unidadeDeMedida = obterValor("Insira a unidade de medida (por exemplo, cm, m, etc.): ");
+            double comprimento = obterValor("Insira o comprimento: ");
+            double largura = obterValor("Insira a largura: ");
+            double altura = obterValor("Insira a altura: ");
+            double calcVolume = comprimento * largura * altura;
+
+
+            Console.WriteLine($"O volume do objeto é de {calcVolume.ToString("F")} {unidadeDeMedida}³");
+
+        }
+
+        static double obterValor(string texto)
+        {
+            Console.WriteLine(texto);
+
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            return input;
+
+        }
+    }
+}
